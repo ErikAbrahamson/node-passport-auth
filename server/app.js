7e23d612928fe74171ca2fc5e0bda9217316972e
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-09-16 18:30:56 -0600

    add passport to app

--- server/app.js
+++ server/app.js
@@ -1,4 +1,7 @@
 // *** main dependencies *** //
+var mongoose = require('mongoose');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -7,24 +10,23 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var swig = require('swig');
 
+// *** mongoose connection to mongodb *** //
+mongoose.connect('mongodb://localhost/passport_local_mongoose_express4');
 
 // *** routes *** //
 var routes = require('./routes/index.js');
-
+var users = require('./routes/users');
 
 // *** express instance *** //
 var app = express();
-
 
 // *** view engine *** //
 var swig = new swig.Swig();
 app.engine('html', swig.renderFile);
 app.set('view engine', 'html');
 
-
 // *** static directory *** //
 app.set('views', path.join(__dirname, 'views'));
-
 
 // *** config middleware *** //
 app.use(logger('dev'));
@@ -33,9 +35,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../client/public')));
 
-
 // *** main routes *** //
 app.use('/', routes);
+
+// *** passport init *** //
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(path.join(__dirname, 'public')));
+var Account = require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 
 
 // catch 404 and forward to error handler
